6ff700f6598d4ef7d62d62781f7b788ce78ce452
KellyTTan <60084565+KellyTTan@users.noreply.github.com>
2020-04-21 18:09:54 -1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 
+function initMap() {
+  var aa = document.getElementById('map');
+  var location = new google.maps.LatLng(21.283952, -157.709427);
+  var options = {
+    center: location,
+    zoom: 10,
+    mapTypeTd: google.maps.MapTypeId.SATELLITE,
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_CENTER
+    }
+  };
+  
+  var myMap = new google.maps.Map(aa, options);
+  
+ 
+  
+  
+}
+google.maps.event.addDomListener(window, 'load', init);
+  
+
+///////.   AIzaSyAVGF1Vh1sfAwFrnm3MHu-kfgoycqt1OiA
